4286c19166095fc203b4c62d9feebd83781f1cf6
Asaiah Henson <Hensonasaiah21@gmail.com>
2024-04-21 22:45:42 -0700

    first commit

---
+++ app/dashboard/Invoices/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Invoices() {
+  return (
+    <div>Invoices</div>
+  )
+}
---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Customer() {
+  return (
+    <div>Customer</div>
+  )
+}
